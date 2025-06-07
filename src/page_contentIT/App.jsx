a8742a06793e52ed212627995ab4229c98241275
[heesooHam] <[hhs31249006@gmail.com]>
2025-06-07 17:59:24 +0900

    4

--- src/page_contentIT/App.jsx
+++ src/page_contentIT/App.jsx
@@ -14,11 +14,11 @@ import Home from '../page_others/Home'
 
 function App() {
   return (
-    <Router>
+    <Router basename = {process.env.PUBLIC_URL}>
       <Header/>
       <Routes>
-        <Route path = "HCI/" element = {<Home url = "https://hcms.hallym.ac.kr/info-s/"/>} />
-        <Route path = "HCI/Content" element={<Content />} />
+        <Route path = "/" element = {<Home url = "https://hcms.hallym.ac.kr/info-s/"/>} />
+        <Route path = "/Content" element={<Content />} />
       </Routes>
     </Router>
   )
